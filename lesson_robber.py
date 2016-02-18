121482e5b997d85b174989959bde6a35423656a5
xuyi123454321 <turambar@mail.ustc.edu.cn>
2016-02-18 17:00:18 +0800

    修改：     lesson_robber.py

--- lesson_robber.py
+++ lesson_robber.py
@@ -10,7 +10,8 @@ These usages have been implemented:
 from urllib import request, parse
 from http import cookiejar
 import re
-from PIL import Image
+from PIL import Image # used to show random picture
+import pyquery
 
 header = {
     "Host": "mis.teach.ustc.edu.cn",
@@ -66,14 +67,14 @@ def data_input():
         'check' : '',
     }
 
-    data['userCode'] = input("user code:")
-    data['passWord'] = input("password:")
+    data['userCode'] = input("用户名:")
+    data['passWord'] = input("密码:")
 
     image_file = open("/tmp/random_image", "rb")
     image = Image.open(image_file)
     image.show()
 
-    data['check'] = input("And the check code:")
+    data['check'] = input("验证码:")
 
     return data
 
@@ -84,16 +85,99 @@ def login(opener):
     postdata = parse.urlencode(postdata).encode()
 
     login_res = opener.open(url+"/login.do", postdata)
-    login_page = login_res.read().decode("gb2312")
+    #login_page = login_res.read().decode("gb2312")
 
-    res_file = open("login.html", "w")
-    res_file.write(login_page)
-    res_file.close()
+    #res_file = open("login.html", "w")
+    #res_file.write(login_page)
+    #res_file.close()
+
+def query_lesson(opener):
+    lesson_type = int(input("课程类型 (1-8, 输0了解编号含义 ):"))
+
+    if lesson_type == 0:
+        print("1: 计划内课程")
+        print("2: 自由选修")
+        print("4: 公选课")
+        print("5: 体育选项")
+        print("6: 英语选课")
+        print("8: 重修重考")
+
+    else:
+        while lesson_type not in [1, 2, 4, 5, 6, 8]:
+            lesson_type = input("错误的课程类型～\n课程类型 (1-8, 输0了解编号含义 ):")
+
+    lesson_number = input("课堂号:")
+
+    get_data = {
+        'xnxq': 20152,
+        'seldwdm': 'null',
+        'selkkdw': '',
+        'seyxn': 2015,
+        'seyxq': 2,
+        'queryType': lesson_type,
+        'rkjs': '',
+        'kkdw': '',
+        'kcmc': '',
+        }
+    
+    get_data = parse.urlencode(get_data, encoding = 'gb2312')
+
+    query = opener.open("http://mis.teach.ustc.edu.cn/init_st_xk_dx.do?"+get_data)
+
+    query_page = query.read()
+    query_page = pyquery.PyQuery(query_page)
+
+    table = query_page.find('table#dxkctable1')
+    tr_list = table.find('tr')[1:]
+    parameter = {} # some data to grap from query result
+
+    for tr in tr_list:
+        if lesson_number == tr.findall('td')[1].text.strip():
+            para_text = tr.findall('td')[-1].find('input').values()[-1]
+            para_list = para_text.split('(')[1].split(')')[0].split(',')
+
+            parameter['xnxq'] = 20152
+            parameter['kcbjbh'] = lesson_number
+            parameter['kcid'] = para_list[3][1:-1] # drop "'"
+            parameter['kclb'] = para_list[5][1:-1]
+            parameter['kcsx'] = para_list[6][1:-1]
+            parameter['cxck'] = para_list[9][1:-1]
+            parameter['zylx'] = para_list[10][1:-1]
+            parameter['gxkfl'] = para_list[11][1:-1]
+            parameter['xlh'] = para_list[12][1:-1]
+            parameter['sjpdm'] = para_list[7][1:-1]
+            parameter['kssjdm'] = para_list[8][1:-1]
+            
+            break
+
+    try:
+        parameter['xnxq']
+    except KeyError: # lesson not found
+        print('没找到。。。')
+        return None
+    else:
+        return parse.urlencode(parameter, encoding='gb2312')
+    
+def rob_lesson(opener, para):
+    insert_page = opener.open("http://mis.teach.ustc.edu.cn/xkgcinsert.do?"+para)
+    print(insert_page.read())
 
 def main():
     opener = get_opener(header)
     get_random_image(opener)
     login(opener)
+
+    try:
+        while True:
+            para = query_lesson(opener)
+            if para != None:
+                break
+
+        while rob_lesson(opener, para):
+            pass
+    except KeyboardInterrupt:
+        print('Bye~~')
+        return 1
 
 if __name__ == '__main__':
     main()
